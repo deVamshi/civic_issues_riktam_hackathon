4dde300b41a32ca65479c329007fb8e243ea5390
deVamshi <dev.vamshidhar@gmail.com>
2022-11-26 11:09:53 +0530

    list of issues UI done

--- lib/components/issue_card.dart
+++ lib/components/issue_card.dart
@@ -21,12 +21,55 @@ class IssueCard extends StatelessWidget {
         clipBehavior: Clip.antiAlias,
         child: Column(
           children: [
-            CachedNetworkImage(
-                imageUrl:
-                    "https://images.unsplash.com/photo-1604357209793-fca5dca89f97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: CachedNetworkImageProvider(
+                      "https://images.unsplash.com/photo-1604357209793-fca5dca89f97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+                    ),
+                  ),
+                ),
+              ),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite,
+                        size: 20,
+                      ),
+                      label: Text("234"),
+                    ),
+                    OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.message,
+                        size: 20,
+                      ),
+                      label: Text(
+                        "Chat",
+                      ),
+                    ),
+                    OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.edit,
+                        size: 20,
+                      ),
+                      label: Text(
+                        "Edit",
+                      ),
+                    ),
+                  ],
+                ),
               ],
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Home"),
       ),
