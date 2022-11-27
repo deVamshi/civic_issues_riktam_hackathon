54879ab5b0e63af27d92d713fad79dd87d3ba933
deVamshi <dev.vamshidhar@gmail.com>
2022-11-27 12:01:25 +0530

    all features implemented

--- lib/services/app_db_service.dart
+++ lib/services/app_db_service.dart
@@ -5,8 +5,22 @@ import 'package:civic_issues_riktam_hackathon/models/issue_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class AppDBService {
+  Future<bool> updateStatus(Issue iss, String newStatus) async {
+    iss.status = newStatus;
+    final dbRef = FirebaseFirestore.instance
+        .collection("issues")
+        .withConverter(
+          fromFirestore: Issue.fromFirestore,
+          toFirestore: (Issue issue, options) => issue.toFirestore(),
+        )
+        .doc(iss.id);
+    await dbRef.set(iss);
+    return true;
+  }
+
   Future<List<Comment>> getAllComments(String id) async {
     Query<Comment> ref = FirebaseFirestore.instance
         .collection("comments")
@@ -45,22 +59,6 @@ class AppDBService {
               toFirestore: (Issue issue, _) => issue.toFirestore(),
             );
 
-    // if (onlyOwner) {
-    //   // FirebaseFirestore.instance
-    //   //     .collection("issues")
-    //   //     .withConverter(
-    //   //       fromFirestore: Issue.fromFirestore,
-    //   //       toFirestore: (Issue issue, _) => issue.toFirestore(),
-    //   //     )
-    //   //     .where('email', isEqualTo: FirebaseAuth.instance.currentUser?.email)
-    //   //     .get();
-    // } else {
-    //   ref = FirebaseFirestore.instance.collection("issues").withConverter(
-    //         fromFirestore: Issue.fromFirestore,
-    //         toFirestore: (Issue issue, _) => issue.toFirestore(),
-    //       );
-    // }
-    
     final docSnap = await ref.get();
     final issues = docSnap.docs;
     List<Issue> allIssues = [];
